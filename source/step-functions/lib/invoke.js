18730e03f5aeb7e84d634f1b9369fcba4bac3c54
Matthias Naber <naber.matthias+github@gmail.com>
2022-01-13 14:47:57 +0100

    find unused execution ids when `doPurge`

--- source/step-functions/lib/invoke.js
+++ source/step-functions/lib/invoke.js
@@ -39,7 +39,50 @@ describe('#INVOKE::', () => {
               "arn": "arn:aws:s3:::buzzhub-master-videos-053041861227-eu-west-1"
             },
             "object": {
-              "key": "2021/09/HkpKWVxdF-k3/sozialdemokraten-gewinnen-norwegische-parlamentswahl.mp4",
+              "key": "2021/11/OLQ8AS0JU8jY/angler-begegnet-groesstem-fisch-der-welt-und-staunt-ehrfuerchtig.mp4",
+              "size": 62967683,
+              "eTag": "c5600de05af79ad4172a1c76f0b4542f",
+              "sequencer": "006140AEA2365517B3"
+            }
+          }
+        }
+      ]
+    });
+
+    console.log(data);
+  });
+
+  it("DELETED", async () => {
+    const data = await lambda.handler({
+      "Records": [
+        {
+          "eventVersion": "2.1",
+          "eventSource": "aws:s3",
+          "awsRegion": "eu-west-1",
+          "eventTime": "2021-09-14T14:16:13.453Z",
+          "eventName": "ObjectDeleted:Deleted",
+          "userIdentity": {
+            "principalId": "AWS:AROAQYWMKSJVWY7QFQAZ2:peruggia-sub"
+          },
+          "requestParameters": {
+            "sourceIPAddress": "54.154.128.7"
+          },
+          "responseElements": {
+            "x-amz-request-id": "XQXRMDCN9GZ3NWN2",
+            "x-amz-id-2": "ID5tZU5BEBMHEOqrGLsfS61TQuCJ4/uQM7yWWR/5nHia/wx3O2lqZ2pgQ/HjMgNXskQhyxkQOJqKvUIFChQJ7ztr3vTPq4Yr"
+          },
+          "s3": {
+            "s3SchemaVersion": "1.0",
+            "configurationId": "mp4",
+            "bucket": {
+              "name": "buzzhub-master-videos-053041861227-eu-west-1",
+              "ownerIdentity": {
+                "principalId": "A2JJZGPFKLPHQR"
+              },
+              "arn": "arn:aws:s3:::buzzhub-master-videos-053041861227-eu-west-1"
+            },
+            "object": {
+              "key": "2021/11/OLQ8AS0JU8jY/angler-begegnet-groesstem-fisch-der-welt-und-staunt-ehrfuerchtig.mp4",
               "size": 62967683,
               "eTag": "c5600de05af79ad4172a1c76f0b4542f",
               "sequencer": "006140AEA2365517B3"
